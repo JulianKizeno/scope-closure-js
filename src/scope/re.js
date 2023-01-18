ef98f7b3a0296ec263351bbd939c15289fc1f962
Julian Quiceno <julian.quiceno@accenture.com>
2023-01-18 20:45:58 +0100

    Redeclare Reassign

---
+++ src/scope/re.js
@@ -0,0 +1,36 @@
+//var
+
+var firstName; // Undefined
+firstName = "Julian";
+console.log(firstName);
+
+var lastName = "Quiceno"; // declarar / asignar
+lastName = "Perez"; // reasignar
+console.log(lastName);
+
+var secondName = "David"; // declarando / asignando
+var secondName = "Steven"; // reasignar
+console.log(secondName);
+
+// let
+
+let fruit = "Apple"; // declarar y asignar
+fruit = "Kiwi"; // reasignar
+
+// let fruit = 'Banana'; // Cannot declare block-scoped variable 'fruit'
+// fruit = 'Kiwi';
+
+// Const
+
+const animal = "dog"; // declara y asigna
+// animal = "cat"; // reasignando
+// const animal = 'Snake'
+console.log(animal);
+
+
+const vehicles = []
+vehicles.push('Mazda')
+console.log(vehicles)
+
+vehicles.pop()
+console.log(vehicles)
