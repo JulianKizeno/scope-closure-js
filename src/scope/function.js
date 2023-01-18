0f5a2d9f5badd0448c534b929f2466c86cca6892
Julian Quiceno <julian.quiceno@accenture.com>
2023-01-18 19:49:35 +0100

    function and block scope

---
+++ src/scope/function.js
@@ -0,0 +1,10 @@
+function greeting() {
+    let userName = 'Julian';
+    console.log(userName);
+
+    if(userName === 'Julian'){
+        console.log(`Hello ${userName}!`)
+    }
+}
+
+greeting();
